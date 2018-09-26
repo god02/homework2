65ba6f6bdcc28e8abce840f047eba6abe67f41ed
USER <107021042@gm.asia.edu.tw>
2018-09-26 14:08:37 +0800

    char

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("I am the one");
+        Scanner scn = new Scanner(System.in);
+        char ch = scn.next().charAt(0);
+        System.out.println((int)ch);
     }
 }
